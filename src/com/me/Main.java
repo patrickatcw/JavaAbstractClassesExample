438b531defd30627d765ddf40e47f9853e1f28d0
patrickatcw <pj8392@yahoo.com>
2018-03-28 09:07:19 -0400

    initial run in main, results commented, the list is empty

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -4,8 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //step 21 instance
+        MyLinkedList list = new MyLinkedList(null);
+
+        //step 22 call method
+        list.traverse(list.getRoot());
+
+        //given code
+        //create a string data array to avoid typing loads of additems instructions
+        String stringData = "Columbus Akron Dayton Canton Cleveland Cadiz Lorain Cleveland";
+
+        //step 23 run
+        //result; The list is empty
+
     }
-
-
 
 }
